936c38093a25ac1c3b88f3fd7227b44e3f6c6bce
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-15 11:05:19 -0500

    get projects functional

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,15 @@
 // Write your "projects" router here!
+
+const express = require('express');
+const router = express.Router();
+const Projects = require('./projects-model')
+
+console.log(Projects)
+
+module.exports = router
+
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projects => res.json(projects))
+        .catch(next)
+})
--- api/server.js
+++ api/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const server = express();
 
-server.use(express.json());
+module.exports = server;
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+server.use(express.json());
+
+const projectsRouter = require('./projects/projects-router');
+server.use('/api/projects', projectsRouter)
+
 server.get('/', (req, res) => {
     res.send('<h1>The Server Is Online</h1>')
 })
 
-module.exports = server;
+server.use((error, req, res, next) => {
+    res.status(error.status || 500).json({message: error.message || "internal server error"})
+})
