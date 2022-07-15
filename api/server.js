be13f75fc30ec0698af790ff4635ac5963b3e4fa
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-15 10:40:46 -0500

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.get('/', (req, res) => {
+    res.send('<h1>The Server Is Online</h1>')
+})
 
 module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+require('dotenv').config()
+
+const server = require('./api/server')
+
+const port = process.env.PORT || 9000
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
